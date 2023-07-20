6a56dc664f03730ce12fbe8d5f1b75b274b06815
moody-Jazz <102924428+moody-Jazz@users.noreply.github.com>
2023-07-20 20:53:31 +0530

    Add files via upload
    removed bugs and exceptions in PieceMovement class

--- Chess/src/java_chess/PieceMovement.java
+++ Chess/src/java_chess/PieceMovement.java
@@ -7,7 +7,7 @@ public class PieceMovement {
 		int r = i / 8, c = i % 8;
 		String list = "", oldPiece;
 
-		// All the moves of knight x represent a rank and y represents a file and
+		// All the moves of knight x represent a rank and y represents a file
 		int x[] = { 2, 1, -1, -2, -2, -1, 1, 2 };
 		int y[] = { 1, 2, 2, 1, -1, -2, -2, -1 };
 
@@ -17,8 +17,7 @@ public class PieceMovement {
 			r += x[j];
 			c += y[j];
 
-			if (r >= 0 && c >= 0 && r < 8 && c < 8) {// is move withing the board? if yes then [r][c] represent a
-														// potentially legal move
+			if (r >= 0 && c >= 0 && r < 8 && c < 8) {// is move withing the board?
 				oldPiece = Board.chessBoard[r][c];
 				if (Board.chessBoard[r][c].equals(" ") || Character.isLowerCase(Board.chessBoard[r][c].charAt(0))) {
 					Board.chessBoard[r][c] = "K";
@@ -136,22 +135,23 @@ public class PieceMovement {
 		int r = i / 8, c = i % 8;
 		for (int j = 0; j < 9; j++) {
 			if (j != 4) {
-				try {
-					if (Character.isLowerCase(Board.chessBoard[r - 1 + j / 3][c - 1 + j % 3].charAt(0))
-							|| " ".equals(Board.chessBoard[r - 1 + j / 3][c - 1 + j % 3])) {
-						oldPiece = Board.chessBoard[r - 1 + j / 3][c - 1 + j % 3];
+				int newRow = r - 1 + j / 3;
+				int newCol = c - 1 + j % 3;
+				if (newRow >= 0 && newRow <= 7 && newCol >= 0 && newCol <= 7) {
+					if (Character.isLowerCase(Board.chessBoard[newRow][newCol].charAt(0))
+							|| " ".equals(Board.chessBoard[newRow][newCol])) {
+						oldPiece = Board.chessBoard[newRow][newCol];
 						Board.chessBoard[r][c] = " ";
-						Board.chessBoard[r - 1 + j / 3][c - 1 + j % 3] = "A";
+						Board.chessBoard[newRow][newCol] = "A";
 						int kingTemp = Board.whiteKingPosition;
 						Board.whiteKingPosition = i + (j / 3) * 8 + j % 3 - 9;
 						if (kingSafe()) {
-							list = list + r + c + (r - 1 + j / 3) + (c - 1 + j % 3) + oldPiece;
+							list = list + r + c + newRow + newCol + oldPiece;
 						}
 						Board.chessBoard[r][c] = "A";
-						Board.chessBoard[r - 1 + j / 3][c - 1 + j % 3] = oldPiece;
+						Board.chessBoard[newRow][newCol] = oldPiece;
 						Board.whiteKingPosition = kingTemp;
 					}
-				} catch (Exception e) {
 				}
 			}
 		}
@@ -163,180 +163,151 @@ public class PieceMovement {
 		String list = "", oldPiece;
 		int r = i / 8, c = i % 8;
 		for (int j = -1; j <= 1; j += 2) {
-			try {// capture
-				if (Character.isLowerCase(Board.chessBoard[r - 1][c + j].charAt(0)) && i >= 16) {
-					oldPiece = Board.chessBoard[r - 1][c + j];
-					Board.chessBoard[r][c] = " ";
-					Board.chessBoard[r - 1][c + j] = "P";
-					if (kingSafe()) {
-						list = list + r + c + (r - 1) + (c + j) + oldPiece;
-					}
-					Board.chessBoard[r][c] = "P";
-					Board.chessBoard[r - 1][c + j] = oldPiece;
-				}
-			} catch (Exception e) {
-			}
-			try {// promotion && capture
-				if (Character.isLowerCase(Board.chessBoard[r - 1][c + j].charAt(0)) && i < 16) {
-					String[] temp = { "Q", "R", "B", "K" };
-					for (int k = 0; k < 4; k++) {
-						oldPiece = Board.chessBoard[r - 1][c + j];
-						Board.chessBoard[r][c] = " ";
-						Board.chessBoard[r - 1][c + j] = temp[k];
-						if (kingSafe()) {
-							// column1,column2,captured-piece,new-piece,P
-							list = list + c + (c + j) + oldPiece + temp[k] + "P";
-						}
-						Board.chessBoard[r][c] = "P";
-						Board.chessBoard[r - 1][c + j] = oldPiece;
-					}
-				}
-			} catch (Exception e) {
-			}
-		}
-		try {// move one up
-			if (" ".equals(Board.chessBoard[r - 1][c]) && i >= 16) {
+
+			// move one up
+			if (i >= 16 && Board.chessBoard[r - 1][c].equals(" ")) {
 				oldPiece = Board.chessBoard[r - 1][c];
 				Board.chessBoard[r][c] = " ";
 				Board.chessBoard[r - 1][c] = "P";
-				if (kingSafe()) {
+				if (kingSafe())
 					list = list + r + c + (r - 1) + c + oldPiece;
-				}
 				Board.chessBoard[r][c] = "P";
 				Board.chessBoard[r - 1][c] = oldPiece;
 			}
-		} catch (Exception e) {
-		}
-		try {// promotion && no capture
-			if (" ".equals(Board.chessBoard[r - 1][c]) && i < 16) {
-				String[] temp = { "Q", "R", "B", "K" };
-				for (int k = 0; k < 4; k++) {
-					oldPiece = Board.chessBoard[r - 1][c];
-					Board.chessBoard[r][c] = " ";
-					Board.chessBoard[r - 1][c] = temp[k];
-					if (kingSafe()) {
-						// column1,column2,captured-piece,new-piece,P
-						list = list + c + c + oldPiece + temp[k] + "P";
-					}
-					Board.chessBoard[r][c] = "P";
-					Board.chessBoard[r - 1][c] = oldPiece;
-				}
-			}
-		} catch (Exception e) {
-		}
-		try {// move two up
-			if (" ".equals(Board.chessBoard[r - 1][c]) && " ".equals(Board.chessBoard[r - 2][c]) && i >= 48) {
+
+			// move two up
+			if (i >= 48 && Board.chessBoard[r - 1][c].equals(" ") && Board.chessBoard[r - 2][c].equals(" ")) {
 				oldPiece = Board.chessBoard[r - 2][c];
 				Board.chessBoard[r][c] = " ";
 				Board.chessBoard[r - 2][c] = "P";
-				if (kingSafe()) {
+				if (kingSafe())
 					list = list + r + c + (r - 2) + c + oldPiece;
-				}
 				Board.chessBoard[r][c] = "P";
 				Board.chessBoard[r - 2][c] = oldPiece;
 			}
-		} catch (Exception e) {
+
+			// capture
+			if (c + j >= 0 && c + j <= 7 && Character.isLowerCase(Board.chessBoard[r - 1][c + j].charAt(0))
+					&& i >= 16) {
+				oldPiece = Board.chessBoard[r - 1][c + j];
+				Board.chessBoard[r][c] = " ";
+				Board.chessBoard[r - 1][c + j] = "P";
+				if (kingSafe())
+					list = list + r + c + (r - 1) + (c + j) + oldPiece;
+				Board.chessBoard[r][c] = "P";
+				Board.chessBoard[r - 1][c + j] = oldPiece;
+			}
+
+			// promotion && capture
+			if (c + j >= 0 && c + j <= 7 && Character.isLowerCase(Board.chessBoard[r - 1][c + j].charAt(0)) && i < 16) {
+				String[] temp = { "Q", "R", "B", "K" };
+				for (int k = 0; k < 4; k++) {
+					oldPiece = Board.chessBoard[r - 1][c + j];
+					Board.chessBoard[r][c] = " ";
+					Board.chessBoard[r - 1][c + j] = temp[k];
+					if (kingSafe())
+						// column1,column2,captured-piece,new-piece,P
+						list = list + c + (c + j) + oldPiece + temp[k] + "P";
+					Board.chessBoard[r][c] = "P";
+					Board.chessBoard[r - 1][c + j] = oldPiece;
+				}
+			}
+		}
+
+		// promotion && no capture
+		if (Board.chessBoard[r - 1][c].equals(" ") && i < 16) {
+			String[] temp = { "Q", "R", "B", "K" };
+			for (int k = 0; k < 4; k++) {
+				oldPiece = Board.chessBoard[r - 1][c];
+				Board.chessBoard[r][c] = " ";
+				Board.chessBoard[r - 1][c] = temp[k];
+				if (kingSafe())
+					// column1,column2,captured-piece,new-piece,P
+					list = list + c + c + oldPiece + temp[k] + "P";
+				Board.chessBoard[r][c] = "P";
+				Board.chessBoard[r - 1][c] = oldPiece;
+			}
 		}
 		return list;
+
+		// to do => en passant
 	}
 
 	public static boolean kingSafe() {
-		// bishop/queen
+		int r = Board.whiteKingPosition / 8, c = Board.whiteKingPosition % 8;
 		int temp = 1;
+
+		// check diagonal threats from bishop/queen
 		for (int i = -1; i <= 1; i += 2) {
 			for (int j = -1; j <= 1; j += 2) {
-				try {
-					while (" "
-							.equals(Board.chessBoard[Board.whiteKingPosition / 8 + temp * i][Board.whiteKingPosition % 8
-									+ temp * j])) {
-						temp++;
-					}
-					if ("b".equals(Board.chessBoard[Board.whiteKingPosition / 8 + temp * i][Board.whiteKingPosition % 8
-							+ temp * j])
-							|| "q".equals(
-									Board.chessBoard[Board.whiteKingPosition / 8 + temp * i][Board.whiteKingPosition % 8
-											+ temp * j])) {
-						return false;
-					}
-				} catch (Exception e) {
-				}
+				while (r + temp * i <= 7 && r + temp * i >= 0 && c + temp * j <= 7 && c + temp * j >= 0
+						&& Board.chessBoard[r + temp * i][c + temp * j].equals(" "))
+					temp++;
+				if ((r + temp * i <= 7 && r + temp * i >= 0 && c + temp * j <= 7 && c + temp * j >= 0)
+						&& (Board.chessBoard[r + temp * i][c + temp * j].equals("b")
+								|| Board.chessBoard[r + temp * i][c + temp * j].equals("q")))
+					return false;
 				temp = 1;
 			}
 		}
-		// rook/queen
+
+		// check streight threats from rook/queen
 		for (int i = -1; i <= 1; i += 2) {
-			try {
-				while (" ".equals(
-						Board.chessBoard[Board.whiteKingPosition / 8][Board.whiteKingPosition % 8 + temp * i])) {
-					temp++;
-				}
-				if ("r".equals(Board.chessBoard[Board.whiteKingPosition / 8][Board.whiteKingPosition % 8 + temp * i])
-						|| "q".equals(Board.chessBoard[Board.whiteKingPosition / 8][Board.whiteKingPosition % 8
-								+ temp * i])) {
-					return false;
-				}
-			} catch (Exception e) {
-			}
+			// check threats in a rank
+			while (c + temp * i >= 0 && c + temp * i <= 7 && Board.chessBoard[r][c + temp * i].equals(" "))
+				temp++;
+			if ((c + temp * i >= 0 && c + temp * i <= 7)
+					&& (Board.chessBoard[r][c + temp * i].equals("r") || Board.chessBoard[r][c + temp * i].equals("q")))
+				return false;
 			temp = 1;
-			try {
-				while (" ".equals(
-						Board.chessBoard[Board.whiteKingPosition / 8 + temp * i][Board.whiteKingPosition % 8])) {
-					temp++;
-				}
-				if ("r".equals(Board.chessBoard[Board.whiteKingPosition / 8 + temp * i][Board.whiteKingPosition % 8])
-						|| "q".equals(Board.chessBoard[Board.whiteKingPosition / 8 + temp * i][Board.whiteKingPosition
-								% 8])) {
-					return false;
-				}
-			} catch (Exception e) {
-			}
+
+			// check threats in a file
+			while (r + temp * i >= 0 && r + temp * i <= 7 && Board.chessBoard[r + temp * i][c].equals(" "))
+				temp++;
+			if ((r + temp * i >= 0 && r + temp * i <= 7)
+					&& (Board.chessBoard[r + temp * i][c].equals("r") || Board.chessBoard[r + temp * i][c].equals("q")))
+				return false;
 			temp = 1;
 		}
-		// knight
-		for (int i = -1; i <= 1; i += 2) {
-			for (int j = -1; j <= 1; j += 2) {
-				try {
-					if ("k".equals(
-							Board.chessBoard[Board.whiteKingPosition / 8 + i][Board.whiteKingPosition % 8 + j * 2])) {
-						return false;
-					}
-				} catch (Exception e) {
-				}
-				try {
-					if ("k".equals(
-							Board.chessBoard[Board.whiteKingPosition / 8 + i * 2][Board.whiteKingPosition % 8 + j])) {
-						return false;
-					}
-				} catch (Exception e) {
-				}
+
+		// check threats from a knight
+		// All the moves of knight. x represent a rank and y represents a file
+		int x[] = { 2, 1, -1, -2, -2, -1, 1, 2 };
+		int y[] = { 1, 2, 2, 1, -1, -2, -2, -1 };
+
+		for (int j = 0; j < 8; j++) {
+
+			while (r + x[j] >= 0 && r + x[j] <= 7 && c + y[j] <= 7 && c + y[j] >= 0) {
+
+				if (Board.chessBoard[r + x[j]][c + y[j]].equals("k"))
+					return false;
+				break;
 			}
 		}
-		// pawn
+
+		// check threats from pawn
 		if (Board.whiteKingPosition >= 16) {
-			try {
-				if ("p".equals(Board.chessBoard[Board.whiteKingPosition / 8 - 1][Board.whiteKingPosition % 8 - 1])) {
-					return false;
-				}
-			} catch (Exception e) {
-			}
-			try {
-				if ("p".equals(Board.chessBoard[Board.whiteKingPosition / 8 - 1][Board.whiteKingPosition % 8 + 1])) {
-					return false;
-				}
-			} catch (Exception e) {
-			}
-			// king
-			for (int i = -1; i <= 1; i++) {
-				for (int j = -1; j <= 1; j++) {
-					if (i != 0 || j != 0) {
-						try {
-							if ("a".equals(Board.chessBoard[Board.whiteKingPosition / 8 + i][Board.whiteKingPosition % 8
-									+ j])) {
-								return false;
-							}
-						} catch (Exception e) {
-						}
-					}
+			/*
+			 * here we are checking if white king position is atleast 16 becuase if our
+			 * white king is in the 6th or 7th rank or position <= 16 then it is always safe
+			 * from enemy pawns since pawns can only move forward and all the pawns are on
+			 * the 6th rank at the starting of game
+			 */
+
+			if (c > 0 && Board.chessBoard[r - 1][c - 1].equals("p"))
+				return false;
+
+			if (c < 7 && Board.chessBoard[r - 1][c + 1].equals("p"))
+				return false;
+		}
+
+		// king
+		for (int i = -1; i <= 1; i++) {
+			for (int j = -1; j <= 1; j++) {
+				if (i != 0 || j != 0) {
+					if (r + i >= 0 && r + i <= 7 && c + j >= 0 && c + j <= 7
+							&& Board.chessBoard[r + i][c + j].equals("a"))
+						return false;
 				}
 			}
 		}
