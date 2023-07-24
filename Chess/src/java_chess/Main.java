b0155fb56ad61c42f04cae59307b663c3a0b5d51
moody-Jazz <102924428+moody-Jazz@users.noreply.github.com>
2023-07-24 21:27:07 +0530

    Add files via upload

--- Chess/src/java_chess/Main.java
+++ Chess/src/java_chess/Main.java
@@ -1,12 +1,13 @@
 package java_chess;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javax.swing.*;
 
 public class Main {
-	
-	
-	
+
 	static int humanAsWhite = -1;// 1=human as white, 0=human as black
-	
 
 	public static void main(String[] args) {
 
@@ -14,13 +15,15 @@ public class Main {
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		BoardGUI ui = new BoardGUI();
 		f.add(ui);
-		f.setSize(1000, 1000);
+		Dimension ss = Toolkit.getDefaultToolkit().getScreenSize();
+		Dimension frameSize = new Dimension(610, 635);
+		f.setBounds(ss.width / 2 - frameSize.width / 2, ss.height / 2 - frameSize.height / 2, frameSize.width, frameSize.height);
 		f.setVisible(true);
-	
+
 		Object[] option = { "Computer", "Human" };
 		humanAsWhite = JOptionPane.showOptionDialog(null, "Who should play as white?", "ABC Options",
 				JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, option, option[1]);
-		
+
 		if (humanAsWhite == 0) {
 			Board.makeMove(Engine.alphaBeta(Engine.globalDepth, 1000000, -1000000, "", 0));
 			Board.flipBoard();
@@ -31,5 +34,5 @@ public class Main {
 		 * // for (int i = 0; i < 8; i++) { //
 		 * System.out.println(Arrays.toString(Board.chessBoard[i])); // }
 		 */
-		}
+	}
 }
