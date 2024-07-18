5b97271a0b41f541ba7cd99850dee7ca04ab9cc3
moody-Jazz <102924428+moody-Jazz@users.noreply.github.com>
2024-07-18 19:37:40 +0530

    add bitmask function to generate attack tables

--- C++/src/bitboard.cpp
+++ C++/src/bitboard.cpp
@@ -97,3 +97,148 @@ not_ab_file:
 */const u64 not_ab_file = 18229723555195321596ULL;
 
 //*******************************************************************************************************
+
+// attack table
+u64 pawn_attacks[2][64];
+u64 knight_attacks[64];
+u64 king_attacks[64];
+u64 rook_attacks[64];
+u64 bishop_attacks[64];
+u64 queen_attacks[64];
+
+// generate pawn attacks
+u64 pawn_attack_bitmask(int side, int square){
+    
+    // result attacks bitboard
+    u64 attacks = 0ULL;
+
+    // piece bitboard
+    u64 bitboard = 0ULL;
+
+    //the current position of piece
+    set_bit(bitboard, square);
+
+    //white pawns
+    if(!side){
+        if((bitboard >> 7) & not_a_file) attacks |= (bitboard >> 7);
+        if((bitboard >> 9) & not_h_file) attacks |= (bitboard >> 9);
+    }
+    //black pawns
+    else{
+        if((bitboard << 7) & not_h_file) attacks |= (bitboard << 7);
+        if((bitboard << 9) & not_a_file) attacks |= (bitboard << 9);
+    }
+    // return attack map
+    return attacks;
+
+}
+//generate knight attacks
+u64 knight_attack_bitmask(int square){
+    u64 attacks = 0ULL;
+
+    u64 bitboard = 0ULL;
+
+    // set piece position
+    set_bit(bitboard, square);
+
+    //generate knight attacks
+    if((bitboard >> 17) & not_h_file) attacks |= (bitboard >> 17);
+    if((bitboard >> 15) & not_a_file) attacks |= (bitboard >> 15);
+    if((bitboard >> 10) & not_hg_file) attacks |= (bitboard >> 10);
+    if((bitboard >> 6) & not_ab_file) attacks |= (bitboard >> 6);    
+
+    if((bitboard << 17) & not_a_file) attacks |= (bitboard << 17);
+    if((bitboard << 15) & not_h_file) attacks |= (bitboard << 15);
+    if((bitboard << 10) & not_ab_file) attacks |= (bitboard << 10);
+    if((bitboard << 6) & not_hg_file) attacks |= (bitboard << 6); 
+    return attacks;
+}
+//generate king attacks
+u64 king_attack_bitmask(int square){
+    u64 attacks = 0ULL;
+
+    u64 bitboard = 0ULL; 
+
+    set_bit(bitboard, square);
+
+    if(bitboard >> 8) attacks |= (bitboard >> 8);
+    if((bitboard >> 9) & not_h_file) attacks |= (bitboard >> 9);
+    if((bitboard >> 7) & not_a_file) attacks |= (bitboard >> 7);
+    if((bitboard >> 1) & not_h_file)attacks |= (bitboard >> 1);  
+
+    if(bitboard << 8) attacks |= (bitboard << 8);
+    if((bitboard << 9) & not_a_file) attacks |= (bitboard << 9);
+    if((bitboard << 7) & not_h_file) attacks |= (bitboard << 7);
+    if((bitboard << 1) & not_a_file)attacks |= (bitboard << 1);  
+    
+    return attacks;
+}
+//generate bishop attacks
+u64 bishop_attack_bitmask(int square, u64 block){
+    u64 attacks = 0ULL;
+    int r, f;
+    int tr = square/8;
+    int tf = square % 8; 
+
+    for(r = tr +1, f = tf+1; r<=7 && f<=7; r++, f++){
+        attacks |= (1ULL << (r*8+f));
+        if(block & (1ULL << (r*8+f))) break;
+    } 
+    for(r = tr -1, f = tf+1; r>=0 && f<=7; r--, f++){
+        attacks |= (1ULL << (r*8+f));
+        if(block & (1ULL << (r*8+f))) break;
+    }
+    for(r = tr +1, f = tf-1; r<=7 && f>=0; r++, f--){
+        attacks |= (1ULL << (r*8+f));
+        if(block & (1ULL << (r*8+f))) break;
+    }
+    for(r = tr -1, f = tf-1; r>=0 && f>=0; r--, f--){
+        attacks |= (1ULL << (r*8+f));
+        if(block & (1ULL << (r*8+f))) break;
+    } 
+
+    return attacks;
+}
+//generate rook attacks
+u64 rook_attack_bitmask(int square, u64 ){
+    u64 attacks = 0ULL;
+
+    int r,f;
+    int tr = square / 8;
+    int tf = square % 8;
+
+    for(r = tr+1; r<= 6; r++) attacks |= (1ULL << (r*8 +tf));
+    for(r = tr-1; r>= 1; r--) attacks |= (1ULL << (r*8 +tf));
+    for(f = tf + 1; f<=6; f++) attacks |= (1ULL << (tr *8 +f));
+    for(f = tf - 1; f>=1; f--) attacks |= (1ULL << (tr *8 +f));
+    return attacks;
+}
+
+void init_leapers_attacks(){
+    //loop over every sqare
+    for(int square{}; square<64; square++){
+        // init pawn attack bitmask here
+        pawn_attacks[white][square] = pawn_attack_bitmask(white, square);
+        pawn_attacks[black][square] = pawn_attack_bitmask(black, square);
+
+        // init knight attacks
+        knight_attacks[square] = knight_attack_bitmask(square);
+
+        // init king attacks
+        king_attacks[square] = king_attack_bitmask(square);
+        // init bishop attacks
+        // bishop_attacks[square] = bishop_attack_bitmask(square);
+        // // init rook attacks
+        // rook_attacks[square] = rook_attack_bitmask(square);
+    }
+}
+//*******************************************main********************************************
+int main(){
+    init_leapers_attacks();
+    u64 block = 0ULL;
+    set_bit(block, e5);
+    print_bb(bishop_attack_bitmask(a1,block));
+    // for(int i = 8; i>=1; i--)
+    //cout<<'a'<<i<<", "<<'b'<<i<<", "<<'c'<<i<<", "<<'d'<<i<<", "<<'e'<<i<<", "<<'f'<<i<<", "<<'g'<<i<<", "<<'h'<<i<<", " <<endl;
+    
+}
