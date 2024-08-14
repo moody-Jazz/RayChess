6828eaf060f7a85b2b837d201f759e6ce99bb81c
moody-Jazz <102924428+moody-Jazz@users.noreply.github.com>
2024-08-14 20:24:08 +0530

    added king attack bitmasks and renamed some functions

--- C++/include/piece.hpp
+++ C++/include/piece.hpp
@@ -22,7 +22,7 @@ for example a white pawn that is on b2 will have attack bitmask as
 
 this bitmask can be used to perform some bitmanipulation with the enmy piece bitboard and the friendly piece bitboard to find
 the pseudo legal move. 
-i have used some algorithms which do bitmanipulation to geenrate all this numbers for example to find knight moves i have used:
+i have used some algorithms from piece.cpp which do bitmanipulation to geenrate all this numbers for example to find knight moves i have used:
     u64 knight_attack_bitmask(int square){
         u64 attacks = 0ULL;
 
@@ -104,15 +104,33 @@ static const uint64 knight_attack_bitmask[64] = {
     4679521487814656, 9077567998918656
 };
 
+static const uint64 king_attack_bitmask[64]{
+    770, 1797, 3594, 7188, 14376, 28752, 57504, 49216, 197123, 460039, 920078, 1840156, 3680312, 7360624,  
+    14721248, 12599488, 50463488, 117769984, 235539968, 471079936, 942159872, 1884319744, 3768639488,
+    3225468928, 12918652928, 30149115904, 60298231808, 120596463616, 241192927232, 482385854464, 964771708928,
+    825720045568, 3307175149568, 7718173671424, 15436347342848, 30872694685696, 61745389371392, 123490778742784,
+    246981557485568, 211384331665408, 846636838289408, 1975852459884544, 3951704919769088, 7903409839538176,
+    15806819679076352, 31613639358152704, 63227278716305408, 54114388906344448, 216739030602088448,
+    505818229730443264, 1011636459460886528, 2023272918921773056, 4046545837843546112, 8093091675687092224,
+    16186183351374184448, 13853283560024178688, 144959613005987840, 362258295026614272, 724516590053228544,
+    1449033180106457088, 2898066360212914176, 5796132720425828352, 11592265440851656704, 4665729213955833856
+};
+
 class Piece
 {
 public:
     Piece();
     BitBoard piece_set[12];
-    BitBoard get_knight_attacks(int square);
-    BitBoard get_king_attacks(int square);
-    BitBoard get_bishop_attacks(int square, BitBoard block);
-    BitBoard get_rook_attacks(int square, BitBoard block);
-    BitBoard get_queen_attacks(int square, BitBoard block);
+    
+    // leaper piece attack bitmasks
+    BitBoard pawn_attack_bitmask(int square);
+    BitBoard pawn_push_bitmask(int square);
+    BitBoard knight_attack_bitmask(int square);
+    BitBoard king_attack_bitmask(int square);
+
+    // slider piece attack bitmask
+    BitBoard get_bishop_attacks(int square, uint64 block);
+    BitBoard get_rook_attacks(int square, uint64 block);
+    BitBoard get_queen_attacks(int square, uint64 block);
     BitBoard get_legal_move();
 };
--- C++/src/piece.cpp
+++ C++/src/piece.cpp
@@ -3,162 +3,35 @@
 #include "../include/globalvar.hpp"
 
 Piece::Piece(){
+    /* 
+    all the pieces will be initialised with a decimal value which in is similar to initial chess pieces position
+
+    White pawn bitboard                 black pawn bitboard                 black rook bitboard
+
+    8  0 0 0 0 0 0 0 0                  8  0 0 0 0 0 0 0 0                  8  1 0 0 0 0 0 0 1            
+    7  0 0 0 0 0 0 0 0                  7  1 1 1 1 1 1 1 1                  7  0 0 0 0 0 0 0 0 
+    6  0 0 0 0 0 0 0 0                  6  0 0 0 0 0 0 0 0                  6  0 0 0 0 0 0 0 0 
+    5  0 0 0 0 0 0 0 0                  5  0 0 0 0 0 0 0 0                  5  0 0 0 0 0 0 0 0 
+    4  0 0 0 0 0 0 0 0                  4  0 0 0 0 0 0 0 0                  4  0 0 0 0 0 0 0 0 
+    3  0 0 0 0 0 0 0 0                  3  0 0 0 0 0 0 0 0                  3  0 0 0 0 0 0 0 0
+    2  1 1 1 1 1 1 1 1                  2  0 0 0 0 0 0 0 0                  2  0 0 0 0 0 0 0 0
+    1  0 0 0 0 0 0 0 0                  1  0 0 0 0 0 0 0 0                  1  0 0 0 0 0 0 0 0
+
+       a b c d e f g h                     a b c d e f g h                     a b c d e f g h
+    */
+
     piece_set[P].set_val(65280ULL);
-    /*  White pawn bitboard
-        8  0 0 0 0 0 0 0 0 
-        7  0 0 0 0 0 0 0 0 
-        6  0 0 0 0 0 0 0 0 
-        5  0 0 0 0 0 0 0 0 
-        4  0 0 0 0 0 0 0 0 
-        3  0 0 0 0 0 0 0 0
-        2  1 1 1 1 1 1 1 1 
-        1  0 0 0 0 0 0 0 0
-
-        a b c d e f g h
-    */
     piece_set[N].set_val(66ULL);
-    /*  white knight bitboard
-        8  0 0 0 0 0 0 0 0 
-        7  0 0 0 0 0 0 0 0 
-        6  0 0 0 0 0 0 0 0 
-        5  0 0 0 0 0 0 0 0 
-        4  0 0 0 0 0 0 0 0 
-        3  0 0 0 0 0 0 0 0
-        2  0 0 0 0 0 0 0 0
-        1  0 1 0 0 0 0 1 0 
-
-           a b c d e f g h
-    */
-   piece_set[B].set_val(36ULL);
-    /*  white bishop bitboard
-        8  0 0 0 0 0 0 0 0 
-        7  0 0 0 0 0 0 0 0 
-        6  0 0 0 0 0 0 0 0 
-        5  0 0 0 0 0 0 0 0 
-        4  0 0 0 0 0 0 0 0 
-        3  0 0 0 0 0 0 0 0
-        2  0 0 0 0 0 0 0 0
-        1  0 0 1 0 0 1 0 0
-        
-           a b c d e f g h
-    */
+    piece_set[B].set_val(36ULL);
     piece_set[R].set_val(129ULL);
-    /*  white rook bitboard
-        8  0 0 0 0 0 0 0 0 
-        7  0 0 0 0 0 0 0 0 
-        6  0 0 0 0 0 0 0 0 
-        5  0 0 0 0 0 0 0 0 
-        4  0 0 0 0 0 0 0 0 
-        3  0 0 0 0 0 0 0 0
-        2  0 0 0 0 0 0 0 0
-        1  1 0 0 0 0 0 0 1
-        
-           a b c d e f g h
-    */
     piece_set[Q].set_val(16ULL);
-    /*  white queen bitboard
-        8  0 0 0 0 0 0 0 0 
-        7  0 0 0 0 0 0 0 0 
-        6  0 0 0 0 0 0 0 0 
-        5  0 0 0 0 0 0 0 0 
-        4  0 0 0 0 0 0 0 0 
-        3  0 0 0 0 0 0 0 0
-        2  0 0 0 0 0 0 0 0
-        1  0 0 0 1 0 0 0 0
-        
-           a b c d e f g h
-    */
     piece_set[K].set_val(8ULL);
-    /*  white king bitboard
-        8  0 0 0 0 0 0 0 0 
-        7  0 0 0 0 0 0 0 0 
-        6  0 0 0 0 0 0 0 0 
-        5  0 0 0 0 0 0 0 0 
-        4  0 0 0 0 0 0 0 0 
-        3  0 0 0 0 0 0 0 0
-        2  0 0 0 0 0 0 0 0
-        1  0 0 0 0 1 0 0 0
-        
-           a b c d e f g h
-    */
     piece_set[p].set_val(71776119061217280ULL);
-    /*  black pawn bitboard
-        8  0 0 0 0 0 0 0 0 
-        7  1 1 1 1 1 1 1 1 
-        6  0 0 0 0 0 0 0 0 
-        5  0 0 0 0 0 0 0 0 
-        4  0 0 0 0 0 0 0 0 
-        3  0 0 0 0 0 0 0 0
-        2  0 0 0 0 0 0 0 0
-        1  0 0 0 0 0 0 0 0
-        
-           a b c d e f g h
-    */
     piece_set[n].set_val(4755801206503243776ULL);
-    /*  black knight bitboard
-        8  0 1 0 0 0 0 1 0 
-        7  0 0 0 0 0 0 0 0 
-        6  0 0 0 0 0 0 0 0 
-        5  0 0 0 0 0 0 0 0 
-        4  0 0 0 0 0 0 0 0 
-        3  0 0 0 0 0 0 0 0
-        2  0 0 0 0 0 0 0 0
-        1  0 0 0 0 0 0 0 0
-        
-           a b c d e f g h
-    */
     piece_set[b].set_val(2594073385365405696ULL);
-    /*  black bishop bitboard
-        8  0 0 1 0 0 1 0 0 
-        7  0 0 0 0 0 0 0 0 
-        6  0 0 0 0 0 0 0 0 
-        5  0 0 0 0 0 0 0 0 
-        4  0 0 0 0 0 0 0 0 
-        3  0 0 0 0 0 0 0 0
-        2  0 0 0 0 0 0 0 0
-        1  0 0 0 0 1 0 0 0
-        
-           a b c d e f g h
-    */
     piece_set[r].set_val(9295429630892703744ULL);
-    /*  black rook bitboard
-        8  1 0 0 0 0 0 0 1 
-        7  0 0 0 0 0 0 0 0 
-        6  0 0 0 0 0 0 0 0 
-        5  0 0 0 0 0 0 0 0 
-        4  0 0 0 0 0 0 0 0 
-        3  0 0 0 0 0 0 0 0
-        2  0 0 0 0 0 0 0 0
-        1  0 0 0 0 1 0 0 0
-        
-           a b c d e f g h
-    */
     piece_set[q].set_val(1152921504606846976ULL);
-    /*  black queen bitboard
-        8  0 0 0 1 0 0 0 0 
-        7  0 0 0 0 0 0 0 0 
-        6  0 0 0 0 0 0 0 0 
-        5  0 0 0 0 0 0 0 0 
-        4  0 0 0 0 0 0 0 0 
-        3  0 0 0 0 0 0 0 0
-        2  0 0 0 0 0 0 0 0
-        1  0 0 0 0 1 0 0 0
-        
-           a b c d e f g h
-    */
     piece_set[k].set_val(576460752303423488ULL);
-    /*  black king bitboard
-        8  0 0 0 0 1 0 0 0 
-        7  0 0 0 0 0 0 0 0 
-        6  0 0 0 0 0 0 0 0 
-        5  0 0 0 0 0 0 0 0 
-        4  0 0 0 0 0 0 0 0 
-        3  0 0 0 0 0 0 0 0
-        2  0 0 0 0 0 0 0 0
-        1  0 0 0 0 0 0 0 0
-        
-           a b c d e f g h
-    */
 }
 
 BitBoard pawn_attack_bitmask(int side, int square){
@@ -185,8 +58,41 @@ BitBoard pawn_attack_bitmask(int side, int square){
     // return attack bitboard
     return attacks;
 }
+BitBoard pawn_push_bitmask(int side, int square){
+    BitBoard attacks(0ULL);
+
+    // piece bitboard
+    BitBoard bitboard(0ULL);
+
+    //the current position of piece
+    bitboard.set_bit(square);
+
+    //black pawns
+    if(side){
+        // if pawn is on initial square then it can move two tiles ahead
+        if(square >=48 && square <= 55){
+            attacks.val |= (bitboard.val >> 8);
+            attacks.val |= (bitboard.val >> 16); 
+        }
+        else {
+            attacks.val |= (bitboard.val >> 8);
+        }
+    }
+    //white pawns
+    else{
+        if(square >=8 && square <= 15){
+            attacks.val |= (bitboard.val << 8);
+            attacks.val |= (bitboard.val << 16); 
+        }
+        else {
+            attacks.val |= (bitboard.val << 8);
+        }
+    }
+    // return attack bitboard
+    return attacks;
+}
 //generate knight attacks
-BitBoard Piece::get_knight_attacks(int square){
+BitBoard Piece::knight_attack_bitmask(int square){
     BitBoard attacks(0ULL);
 
     BitBoard bitboard(0ULL);
@@ -207,7 +113,7 @@ BitBoard Piece::get_knight_attacks(int square){
     return attacks;
 }
 //generate king attacks
-BitBoard Piece::get_king_attacks(int square){
+BitBoard Piece::king_attack_bitmask(int square){
     BitBoard attacks(0ULL);
 
     BitBoard bitboard(0ULL); 
@@ -225,7 +131,7 @@ BitBoard Piece::get_king_attacks(int square){
     if((bitboard.val << 1) & not_a_file)attacks.val |= (bitboard.val << 1);  
     return attacks;
 }
-BitBoard Piece::get_bishop_attacks(int square, BitBoard block){
+BitBoard Piece::get_bishop_attacks(int square, uint64 block){
     BitBoard attacks(0ULL);
     int r, f;
     int tr = square/8;
@@ -233,24 +139,24 @@ BitBoard Piece::get_bishop_attacks(int square, BitBoard block){
 
     for(r = tr +1, f = tf+1; r<=7 && f<=7; r++, f++){
         attacks.val |= (1ULL << (r*8+f));
-        if(block.val & (1ULL << (r*8+f))) break;
+        if(block & (1ULL << (r*8+f))) break;
     } 
     for(r = tr -1, f = tf+1; r>=0 && f<=7; r--, f++){
         attacks.val |= (1ULL << (r*8+f));
-        if(block.val & (1ULL << (r*8+f))) break;
+        if(block & (1ULL << (r*8+f))) break;
     }
     for(r = tr +1, f = tf-1; r<=7 && f>=0; r++, f--){
         attacks.val |= (1ULL << (r*8+f));
-        if(block.val & (1ULL << (r*8+f))) break;
+        if(block & (1ULL << (r*8+f))) break;
     }
     for(r = tr -1, f = tf-1; r>=0 && f>=0; r--, f--){
         attacks.val |= (1ULL << (r*8+f));
-        if(block.val & (1ULL << (r*8+f))) break;
+        if(block & (1ULL << (r*8+f))) break;
     } 
 
     return attacks;
 }
-BitBoard Piece::get_rook_attacks(int square, BitBoard block){
+BitBoard Piece::get_rook_attacks(int square, uint64 block){
     BitBoard attacks(0ULL);
 
     int r,f;
@@ -259,24 +165,24 @@ BitBoard Piece::get_rook_attacks(int square, BitBoard block){
 
     for(r = tr+1; r<= 7; r++) {
         attacks.val |= (1ULL << (r*8 +tf));
-        if((1ULL << (r*8 +tf)) & block.val) break;
+        if((1ULL << (r*8 +tf)) & block) break;
 
     }
     for(r = tr-1; r>= 0; r--) {
         attacks.val |= (1ULL << (r*8 +tf));
-        if((1ULL << (r*8 +tf)) & block.val) break;
+        if((1ULL << (r*8 +tf)) & block) break;
     }
     for(f = tf + 1; f<=7; f++) {
         attacks.val |= (1ULL << (tr *8 +f));
-        if((1ULL << (tr *8 +f)) & block.val) break;
+        if((1ULL << (tr *8 +f)) & block) break;
     }
     for(f = tf - 1; f>=0; f--) {
         attacks.val |= (1ULL << (tr *8 +f));
-        if((1ULL << (tr *8 +f)) & block.val) break;
+        if((1ULL << (tr *8 +f)) & block) break;
     }
     return attacks.val;
 }
-BitBoard Piece::get_queen_attacks(int square, BitBoard block){
+BitBoard Piece::get_queen_attacks(int square, uint64 block){
     BitBoard attacks(0ULL);
     attacks.val |= get_bishop_attacks(square, block).val;
     attacks.val |= get_rook_attacks(square, block).val;
